1a1ed4f00cb70190cdde57b57146adfa6e72b3fb
Evan Otero <Evan.otero19@gmail.com>
2019-12-13 21:33:55 +0000

    initial commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,65 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container">
+		<div className="row">
+			<div className="card col-md-4">
+				<div className="imgdiv">
+					<img
+						className="card-img-top"
+						src="https://secureimg.stitcher.com/feedimagesplain328/78814.jpg"
+						alt="Card image cap"
+					/>
+				</div>
+				<div className="card-body">
+					<h5 className="card-title">Ep 75: Blade Runner (Part 2)</h5>
+					<h8>
+						Blade Runner continues! Courtney gets a hat! Carolyn gets a wig! Lisa drinks Champagne! Pitr
+						drinks Zima! Paulo pets a cat!
+					</h8>
+					{/* <p className="card-text">
+						Some quick example text to build on the card title and make up the bulk of the card's content.
+					</p> */}
+				</div>
+			</div>
+			<div className="card col-md-4">
+				<div className="imgdiv">
+					<img
+						className="card-img-top"
+						src="https://secureimg.stitcher.com/feedimagesplain328/78814.jpg"
+						alt="Card image cap"
+					/>
+				</div>
+				<div className="card-body">
+					<h5 className="card-title">Ep 74: Blade Runner (Part 1)</h5>
+					<h8>
+						We run some blades! Pitr gets a job! Lisa drinks champagne! Courtney does some snooping! Paulo
+						has soup!
+					</h8>
+					{/* <p className="card-text">
+						Some quick example text to build on the card title and make up the bulk of the card's content.
+					</p> */}
+				</div>
+			</div>
+			<div className="card col-md-4">
+				<div className="imgdiv">
+					<img
+						className="card-img-top"
+						src="https://secureimg.stitcher.com/feedimagesplain328/78814.jpg"
+						alt="Card image cap"
+					/>
+				</div>
+				<div className="card-body">
+					<h5 className="card-title">Ep 73: Adventure Time</h5>
+					<h8>
+						We play through the Elements saga of Adventure Time! Andy loses himself! Pitr finds himself! Jon
+						embraces his inner Cthulhu!
+					</h8>
+					{/* <p className="card-text">
+						Some quick example text to build on the card title and make up the bulk of the card's content.
+					</p> */}
+				</div>
+			</div>
+		</div>
 	</div>
 );
